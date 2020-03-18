549a067aff3142976aee49c1409ade7414118ec0
Jordan Micah Bennett <jordanmicahbennett@gmail.com>
2020-03-18 14:47:23 -0500

    Update covid19_ai_diagnoser.py

--- covid19_ai_diagnoser.py
+++ covid19_ai_diagnoser.py
@@ -3,20 +3,20 @@
 import covid19_ai_diagnoser_optimal_model_architecture
 
 
-model = covid19_ai_diagnoser_optimal_model_architecture.model
-
+model_pneumoniaDetector = covid19_ai_diagnoser_optimal_model_architecture.model_pneumoniaDetector
+model_covid19PneumoniaDetector = covid19_ai_diagnoser_optimal_model_architecture.model_covid19PneumoniaDetector
 
 
 ###########################
-#Function written by Jordan to simply collate prediction given image path, based on original code https://github.com/JohnChangUK/Pneumonia-Kaggle/blob/master/test_model_prediction.ipynb
-def doOnlineInference (imagePath):
+#Function written by Jordan to do online inference i.e. Regular Pneumonia tests
+def doOnlineInference_regularPneumonia (imagePath):
     test_data = []
     img = covid19_ai_diagnoser_optimal_model_architecture.cv2.imread(imagePath,0) #Replace plt.imread, with  gray scale cv2.imread(path,0), so that ui's image load process doesn't throw a pyimage10 error
     img = covid19_ai_diagnoser_optimal_model_architecture.cv2.resize(img, (covid19_ai_diagnoser_optimal_model_architecture.img_dims, covid19_ai_diagnoser_optimal_model_architecture.img_dims))
     img = covid19_ai_diagnoser_optimal_model_architecture.np.dstack([img, img, img])
     img = img.astype('float32') / 255
     test_data.append(img)
-    prediction = model.predict(covid19_ai_diagnoser_optimal_model_architecture.np.array(test_data))
+    prediction = model_pneumoniaDetector.predict(covid19_ai_diagnoser_optimal_model_architecture.np.array(test_data))
     _prediction = round( prediction[0][0]*100, 3 )
     if ( _prediction > 50 ):
         _prediction = "Pneumonia detected";
@@ -26,6 +26,27 @@ def doOnlineInference (imagePath):
     outputContent += "Raw Neural Network Output : " + str(prediction[0][0]) + "\n\n"
     recordInferenceEvent (imagePath, outputContent)
     return outputContent
+
+
+#Function written by Jordan to do online inference i.e. Covid19 tests
+def doOnlineInference_covid19Pneumonia (imagePath):
+    test_data = []
+    img = covid19_ai_diagnoser_optimal_model_architecture.cv2.imread(imagePath,0) #Replace plt.imread, with  gray scale cv2.imread(path,0), so that ui's image load process doesn't throw a pyimage10 error
+    img = covid19_ai_diagnoser_optimal_model_architecture.cv2.resize(img, (covid19_ai_diagnoser_optimal_model_architecture.img_dims, covid19_ai_diagnoser_optimal_model_architecture.img_dims))
+    img = covid19_ai_diagnoser_optimal_model_architecture.np.dstack([img, img, img])
+    img = img.astype('float32') / 255
+    test_data.append(img)
+    prediction = model_covid19PneumoniaDetector.predict(covid19_ai_diagnoser_optimal_model_architecture.np.array(test_data))
+    _prediction = round( prediction[0][0]*100, 3 )
+    if ( _prediction > 50 ):
+        _prediction = "Pneumonia detected";
+    elif ( _prediction < 50 ):
+        _prediction = "Normal lungs detected";  
+    outputContent = _prediction + "\n"
+    outputContent += "Raw Neural Network Output : " + str(prediction[0][0]) + "\n\n"
+    recordInferenceEvent (imagePath, outputContent)
+    return outputContent
+
 
 
 #Record each inference in a text file 
@@ -41,16 +62,16 @@ def recordInferenceEvent ( imagePath, outputContent ):
 
 """
 NORMAL SAMPLES:
-doOnlineInference("xray_dataset/val/NORMAL/NORMAL2-IM-1430-0001.jpeg")
-doOnlineInference("xray_dataset/val/NORMAL/NORMAL2-IM-1427-0001.jpeg")
+doOnlineInference_regularPneumonia("xray_dataset/val/NORMAL/NORMAL2-IM-1430-0001.jpeg")
+doOnlineInference_regularPneumonia("xray_dataset/val/NORMAL/NORMAL2-IM-1427-0001.jpeg")
 
 PNEUMONIA SAMPLES:
-doOnlineInference("xray_dataset/val/PNEUMONIA/person1946_bacteria_4875.jpeg")
-doOnlineInference("xray_dataset/val/PNEUMONIA/person1950_bacteria_4881.jpeg")
+doOnlineInference_regularPneumonia("xray_dataset/val/PNEUMONIA/person1946_bacteria_4875.jpeg")
+doOnlineInference_regularPneumonia("xray_dataset/val/PNEUMONIA/person1950_bacteria_4881.jpeg")
 
 ACTUAL CORONAVIRUS SAMPLES:
-doOnlineInference("coronavirus_positive_WeifangKong_et-al.jpg")
-doOnlineInference("coronavirus_positive_day7_of_infection_UPSCALED.jpg")
+doOnlineInference_regularPneumonia("coronavirus_positive_WeifangKong_et-al.jpg")
+doOnlineInference_regularPneumonia("coronavirus_positive_day7_of_infection_UPSCALED.jpg")
 """
 
 
