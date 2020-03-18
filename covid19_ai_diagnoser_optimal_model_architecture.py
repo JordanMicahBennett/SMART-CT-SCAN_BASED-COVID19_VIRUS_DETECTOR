732db163d9d2f1db0b33b016549b14a1e8fb8907
Jordan Micah Bennett <jordanmicahbennett@gmail.com>
2020-03-17 20:43:10 -0500

    Add files via upload

---
+++ covid19_ai_diagnoser_optimal_model_architecture.py
@@ -0,0 +1,225 @@
+#Original code: https://www.kaggle.com/gbellport/pneumonia-detection-96-recall-91-accuracy/output
+#Code modified by Jordan Bennett, and converted from .pynb to .py using https://jupyter.org/try
+
+
+#################################
+##Define model architecture
+
+
+# General libraries
+import os
+import numpy as np
+import random
+import cv2
+import matplotlib.pyplot as plt
+
+
+# Deep learning libraries
+import keras.backend as K
+from keras.models import Model, Sequential
+from keras.layers import Input, Dense, Flatten, Dropout, BatchNormalization
+from keras.layers import Conv2D, SeparableConv2D, MaxPool2D, LeakyReLU, Activation
+from keras.optimizers import Adam
+from keras.preprocessing.image import ImageDataGenerator
+from keras.callbacks import ModelCheckpoint, ReduceLROnPlateau, EarlyStopping
+import tensorflow as tf
+
+# Setting seeds for reproducibility
+seed = 232
+np.random.seed(seed)
+tf.set_random_seed(seed)
+
+
+# Lets first look at some of our X-ray images and how each dataset is distributed:
+
+# In[2]:
+
+
+# input_path = '../input/chest_xray/chest_xray/'
+input_path = 'xray_dataset/'
+
+fig, ax = plt.subplots(2, 3, figsize=(15, 7))
+ax = ax.ravel()
+plt.tight_layout()
+
+for i, _set in enumerate(['train', 'val', 'test']):
+    set_path = input_path+_set
+    ax[i].imshow(plt.imread(set_path+'/NORMAL/'+os.listdir(set_path+'/NORMAL')[0]), cmap='gray')
+    ax[i].set_title('Set: {}, Condition: Normal'.format(_set))
+    ax[i+3].imshow(plt.imread(set_path+'/PNEUMONIA/'+os.listdir(set_path+'/PNEUMONIA')[0]), cmap='gray')
+    ax[i+3].set_title('Set: {}, Condition: Pneumonia'.format(_set))
+
+
+# In[3]:
+
+
+# Distribution of our datasets
+for _set in ['train', 'val', 'test']:
+    n_normal = len(os.listdir(input_path + _set + '/NORMAL'))
+    n_infect = len(os.listdir(input_path + _set + '/PNEUMONIA'))
+    print('Set: {}, normal images: {}, pneumonia images: {}'.format(_set, n_normal, n_infect))
+
+
+# There is too little validation data for it to provide any reliable insight, for this reason I will be using the test set for validation.
+
+# # Preparing the data for the model
+# 
+# Using Keras' `ImageDataGenerator()` and `.flow_from_directory()` we can feed the data in batches directly from the directory since it is already organized by class while also feeding augmented copies of the data. This ensures we do not run out of memory while training the model.
+
+# In[4]:
+
+
+# input_path = '../input/chest_xray/chest_xray/'
+input_path = 'xray_dataset/'
+
+def process_data(img_dims, batch_size):
+    # Data generation objects
+    train_datagen = ImageDataGenerator(rescale=1./255, zoom_range=0.3, vertical_flip=True)
+    test_val_datagen = ImageDataGenerator(rescale=1./255)
+    
+    # This is fed to the network in the specified batch sizes and image dimensions
+    train_gen = train_datagen.flow_from_directory(
+    directory=input_path+'train', 
+    target_size=(img_dims, img_dims), 
+    batch_size=batch_size, 
+    class_mode='binary', 
+    shuffle=True)
+
+    test_gen = test_val_datagen.flow_from_directory(
+    directory=input_path+'test', 
+    target_size=(img_dims, img_dims), 
+    batch_size=batch_size, 
+    class_mode='binary', 
+    shuffle=True)
+    
+    # I will be making predictions off of the test set in one batch size
+    # This is useful to be able to get the confusion matrix
+    test_data = []
+    test_labels = []
+
+    for cond in ['/NORMAL/', '/PNEUMONIA/']:
+        for img in (os.listdir(input_path + 'test' + cond)):
+            img = plt.imread(input_path+'test'+cond+img)
+            img = cv2.resize(img, (img_dims, img_dims))
+            img = np.dstack([img, img, img])
+            img = img.astype('float32') / 255
+            if cond=='/NORMAL/':
+                label = 0
+            elif cond=='/PNEUMONIA/':
+                label = 1
+            test_data.append(img)
+            test_labels.append(label)
+        
+    test_data = np.array(test_data)
+    test_labels = np.array(test_labels)
+    
+    return train_gen, test_gen, test_data, test_labels
+
+
+# # The Model
+# 
+# I would like to acknowledge that this Kaggle kernel: https://www.kaggle.com/aakashnain/beating-everything-with-depthwise-convolution motivated me to learn about separable depthwise convolution and implement it in my model. My results were instantly better when swapping out the convolutional blocks with these types of layers.
+
+# In[5]:
+
+
+# Hyperparameters
+img_dims = 150
+epochs = 10
+batch_size = 32
+
+# Getting the data
+train_gen, test_gen, test_data, test_labels = process_data(img_dims, batch_size)
+
+
+# In[6]:
+
+
+# Input layer
+inputs = Input(shape=(img_dims, img_dims, 3))
+
+# First conv block
+x = Conv2D(filters=16, kernel_size=(3, 3), activation='relu', padding='same')(inputs)
+x = Conv2D(filters=16, kernel_size=(3, 3), activation='relu', padding='same')(x)
+x = MaxPool2D(pool_size=(2, 2))(x)
+
+# Second conv block
+x = SeparableConv2D(filters=32, kernel_size=(3, 3), activation='relu', padding='same')(x)
+x = SeparableConv2D(filters=32, kernel_size=(3, 3), activation='relu', padding='same')(x)
+x = BatchNormalization()(x)
+x = MaxPool2D(pool_size=(2, 2))(x)
+
+# Third conv block
+x = SeparableConv2D(filters=64, kernel_size=(3, 3), activation='relu', padding='same')(x)
+x = SeparableConv2D(filters=64, kernel_size=(3, 3), activation='relu', padding='same')(x)
+x = BatchNormalization()(x)
+x = MaxPool2D(pool_size=(2, 2))(x)
+
+# Fourth conv block
+x = SeparableConv2D(filters=128, kernel_size=(3, 3), activation='relu', padding='same')(x)
+x = SeparableConv2D(filters=128, kernel_size=(3, 3), activation='relu', padding='same')(x)
+x = BatchNormalization()(x)
+x = MaxPool2D(pool_size=(2, 2))(x)
+x = Dropout(rate=0.2)(x)
+
+# Fifth conv block
+x = SeparableConv2D(filters=256, kernel_size=(3, 3), activation='relu', padding='same')(x)
+x = SeparableConv2D(filters=256, kernel_size=(3, 3), activation='relu', padding='same')(x)
+x = BatchNormalization()(x)
+x = MaxPool2D(pool_size=(2, 2))(x)
+x = Dropout(rate=0.2)(x)
+
+# FC layer
+x = Flatten()(x)
+x = Dense(units=512, activation='relu')(x)
+x = Dropout(rate=0.7)(x)
+x = Dense(units=128, activation='relu')(x)
+x = Dropout(rate=0.5)(x)
+x = Dense(units=64, activation='relu')(x)
+x = Dropout(rate=0.3)(x)
+
+# Output layer
+output = Dense(units=1, activation='sigmoid')(x)
+
+# Creating model and compiling
+model = Model(inputs=inputs, outputs=output)
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+
+
+#################################
+##Load saved weights from original author
+model.load_weights('best_weights_kaggle_user_pneumonia2_0.hdf5')
+
+
+
+
+#################################
+##Confusion matrix report/Accuracy measures
+from sklearn.metrics import accuracy_score, confusion_matrix
+
+preds = model.predict(test_data)
+
+acc = accuracy_score(test_labels, np.round(preds))*100
+cm = confusion_matrix(test_labels, np.round(preds))
+tn, fp, fn, tp = cm.ravel()
+
+
+print('\nCONFUSION MATRIX FORMAT ------------------\n')
+print("[true positives    false positives]\n")
+print("[false negatives    true negatives]\n\n")
+
+print('CONFUSION MATRIX ------------------')
+print(cm)
+
+print('\nTEST METRICS ----------------------')
+precision = tp/(tp+fp)*100
+recall = tp/(tp+fn)*100
+specificity = tn/(tn+fp)*100 #Jordan_note: added specificity calculation 
+print('Accuracy: {}%'.format(acc))
+print('Precision: {}%'.format(precision))
+print('Recall/Sensitivity: {}%'.format(recall)) #Jordan_note: added sensitivity label
+print('Specificity {}%'.format(specificity)) #Jordan_note: added specificity calculation 
+print('F1-score: {}'.format(2*precision*recall/(precision+recall)))
+
+
