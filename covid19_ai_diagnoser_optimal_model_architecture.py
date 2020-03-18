f6f6380385feea7e70361ba98708321fb9838526
Jordan Micah Bennett <jordanmicahbennett@gmail.com>
2020-03-18 14:45:24 -0500

    Update covid19_ai_diagnoser_optimal_model_architecture.py

--- covid19_ai_diagnoser_optimal_model_architecture.py
+++ covid19_ai_diagnoser_optimal_model_architecture.py
@@ -1,221 +1,104 @@
-#Original code: https://www.kaggle.com/gbellport/pneumonia-detection-96-recall-91-accuracy/output
-#Code modified by Jordan Bennett , and converted from .pynb to .py using https://jupyter.org/try
+#Author: Jordan Bennett
+#Note: Simple ui to use Ai based coronavirus2019/covid19 diagnosis tool
 
-######################
-# Jordan's modifications : 
-#1. Model training components removed. (Saved weight loading takes place instead)
-#2. Replace pyplot -> plt.imread, with  gray scale cv2.imread(path,0), so that ui's image load process doesn't throw a pyimage10 error
-#3. Other pyplot related code removed.
-#4. Confusion Matrix Format added, as quick explanation of Confusion Matrix that was already included.
-#5. Specificty calculation and relevant label added.
+import covid19_ai_diagnoser
 
-#################################
-##Define model architecture
+from tkinter import Frame, Tk, BOTH, Label, Menu, filedialog, messagebox
+from PIL import Image, ImageTk
 
-
-# General libraries
 import os
-import numpy as np
-import random
+import codecs
+
+screenWidth = "1560"
+screenHeight = "840"
+windowTitle = "Smart/Ai Coronavirus 2019 (Covid19) Diagnosis Tool"
+
 import cv2
 
-
-
-# Deep learning libraries
-import keras.backend as K
-from keras.models import Model, Sequential
-from keras.layers import Input, Dense, Flatten, Dropout, BatchNormalization
-from keras.layers import Conv2D, SeparableConv2D, MaxPool2D, LeakyReLU, Activation
-from keras.optimizers import Adam
-from keras.preprocessing.image import ImageDataGenerator
-from keras.callbacks import ModelCheckpoint, ReduceLROnPlateau, EarlyStopping
-import tensorflow as tf
-
-# Setting seeds for reproducibility
-seed = 232
-np.random.seed(seed)
-tf.set_random_seed(seed)
-
-
-# Lets first look at some of our X-ray images and how each dataset is distributed:
-
-# In[2]:
-
-
-# input_path = '../input/chest_xray/chest_xray/'
-input_path = 'xray_dataset/'
-
-
-# In[3]:
-
-
-# Distribution of our datasets
-for _set in ['train', 'val', 'test']:
-    n_normal = len(os.listdir(input_path + _set + '/NORMAL'))
-    n_infect = len(os.listdir(input_path + _set + '/PNEUMONIA'))
-    print('Set: {}, normal images: {}, pneumonia images: {}'.format(_set, n_normal, n_infect))
-
-
-# There is too little validation data for it to provide any reliable insight, for this reason I will be using the test set for validation.
-
-# # Preparing the data for the model
-# 
-# Using Keras' `ImageDataGenerator()` and `.flow_from_directory()` we can feed the data in batches directly from the directory since it is already organized by class while also feeding augmented copies of the data. This ensures we do not run out of memory while training the model.
-
-# In[4]:
-
-
-# input_path = '../input/chest_xray/chest_xray/'
-input_path = 'xray_dataset/'
-
-def process_data(img_dims, batch_size):
-    # Data generation objects
-    train_datagen = ImageDataGenerator(rescale=1./255, zoom_range=0.3, vertical_flip=True)
-    test_val_datagen = ImageDataGenerator(rescale=1./255)
+class Window(Frame):
+    _PRIOR_IMAGE = None
+    #establish variable to keep track of images added to Frame, for purpose of preventing stacking @ new image additions
+    #by using destroy() on each old image instance @ addition
+	#Added by Jordan Bennett, based on suggestion by Andrei Marinescu, who suggested that xray images should not stack as new ones are loaded. (https://www.facebook.com/mvandrei)
     
-    # This is fed to the network in the specified batch sizes and image dimensions
-    train_gen = train_datagen.flow_from_directory(
-    directory=input_path+'train', 
-    target_size=(img_dims, img_dims), 
-    batch_size=batch_size, 
-    class_mode='binary', 
-    shuffle=True)
-
-    test_gen = test_val_datagen.flow_from_directory(
-    directory=input_path+'test', 
-    target_size=(img_dims, img_dims), 
-    batch_size=batch_size, 
-    class_mode='binary', 
-    shuffle=True)
-    
-    # I will be making predictions off of the test set in one batch size
-    # This is useful to be able to get the confusion matrix
-    test_data = []
-    test_labels = []
-
-    for cond in ['/NORMAL/', '/PNEUMONIA/']:
-        for img in (os.listdir(input_path + 'test' + cond)):
-            img = cv2.imread(input_path+'test'+cond+img,0) #Replace plt.imread, with  gray scale cv2.imread(path,0), so that ui's image load process doesn't throw a pyimage10 error
-            img = cv2.resize(img, (img_dims, img_dims))
-            img = np.dstack([img, img, img])
-            img = img.astype('float32') / 255
-            if cond=='/NORMAL/':
-                label = 0
-            elif cond=='/PNEUMONIA/':
-                label = 1
-            test_data.append(img)
-            test_labels.append(label)
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.master = master
+        self.pack(fill=BOTH, expand=1)
         
-    test_data = np.array(test_data)
-    test_labels = np.array(test_labels)
+        load = Image.open("covid19_ai_diagnoser_default__.jpg")
+        render = ImageTk.PhotoImage(load)
+        img = Label(self, image=render)
+        img.image = render
+        img.place(x=(int(screenWidth)/2)-load.width/2, y=((int(screenHeight)/2))-load.height/2)
+        self._PRIOR_IMAGE = img #setup prior image instance
+
+
+        
+root = Tk()
+app = Window(root)
+
+
+
+############
+#screen window size, window title
+root.wm_title(windowTitle)
+root.geometry(screenWidth + "x" + screenHeight)
+
+############
+#menu bar
+menubar = Menu(root)
+
+# Adding a cascade to the menu bar:
+filemenu = Menu(menubar, tearoff=0)
+menubar.add_cascade(label="Files", menu=filemenu)
+
+# Defining function to trigger file browser
+def loadRegularPneumoniaImageFromDialog():
+    currdir = os.getcwd()
+    image_file = filedialog.askopenfile(mode ='r', parent=root, initialdir=currdir, title='Please select an Xray Image of suspected regular pneumonia case:')
+    root.wm_title(windowTitle + " : " + image_file.name)
+    loadRegularPneumoniaImageFromName(image_file.name)
+
+def loadRegularPneumoniaImageFromName(filename):
+    app._PRIOR_IMAGE.destroy() #destroy old image
+    load = Image.open(filename)
+    render = ImageTk.PhotoImage(load)
+    img = Label(image=render)
+    img.image = render
+    img.place(x=(int(screenWidth)/2)-load.width/2, y=((int(screenHeight)/2))-load.height/2)
+    outputContent = "#############################################\n" + filename+"\n\n"
+    outputContent += covid19_ai_diagnoser.doOnlineInference_regularPneumonia (filename)
+    print(outputContent)
+    app._PRIOR_IMAGE = img #set latest instance of old image
+    messagebox.showinfo(title=windowTitle + " : Result ", message=outputContent)
+
+
+def loadCovid19ImageFromDialog():
+    currdir = os.getcwd()
+    image_file = filedialog.askopenfile(mode ='r', parent=root, initialdir=currdir, title='Please select an Xray Image of suspected coronavirus2019 case:')
+    root.wm_title(windowTitle + " : " + image_file.name)
+    loadCovid19ImageFromName(image_file.name)
+
+def loadCovid19ImageFromName(filename):
+    app._PRIOR_IMAGE.destroy() #destroy old image
+    load = Image.open(filename)
+    render = ImageTk.PhotoImage(load)
+    img = Label(image=render)
+    img.image = render
+    img.place(x=(int(screenWidth)/2)-load.width/2, y=((int(screenHeight)/2))-load.height/2)
+    outputContent = "#############################################\n" + filename+"\n\n"
+    outputContent += covid19_ai_diagnoser.doOnlineInference_covid19Pneumonia (filename)
+    print(outputContent)
+    app._PRIOR_IMAGE = img #set latest instance of old image
+    messagebox.showinfo(title=windowTitle + " : Result ", message=outputContent)
     
-    return train_gen, test_gen, test_data, test_labels
 
+# Adding a load image button to the cascade menu "File"
+filemenu.add_command(label="Load Regular Pnuemonia image", command=loadRegularPneumoniaImageFromDialog)
+filemenu.add_command(label="Load Covid19 Pneumonia image", command=loadCovid19ImageFromDialog)
 
-# # The Model
-# 
-# I would like to acknowledge that this Kaggle kernel: https://www.kaggle.com/aakashnain/beating-everything-with-depthwise-convolution motivated me to learn about separable depthwise convolution and implement it in my model. My results were instantly better when swapping out the convolutional blocks with these types of layers.
-
-# In[5]:
-
-
-# Hyperparameters
-img_dims = 150
-epochs = 10
-batch_size = 32
-
-# Getting the data
-train_gen, test_gen, test_data, test_labels = process_data(img_dims, batch_size)
-
-
-# In[6]:
-
-
-# Input layer
-inputs = Input(shape=(img_dims, img_dims, 3))
-
-# First conv block
-x = Conv2D(filters=16, kernel_size=(3, 3), activation='relu', padding='same')(inputs)
-x = Conv2D(filters=16, kernel_size=(3, 3), activation='relu', padding='same')(x)
-x = MaxPool2D(pool_size=(2, 2))(x)
-
-# Second conv block
-x = SeparableConv2D(filters=32, kernel_size=(3, 3), activation='relu', padding='same')(x)
-x = SeparableConv2D(filters=32, kernel_size=(3, 3), activation='relu', padding='same')(x)
-x = BatchNormalization()(x)
-x = MaxPool2D(pool_size=(2, 2))(x)
-
-# Third conv block
-x = SeparableConv2D(filters=64, kernel_size=(3, 3), activation='relu', padding='same')(x)
-x = SeparableConv2D(filters=64, kernel_size=(3, 3), activation='relu', padding='same')(x)
-x = BatchNormalization()(x)
-x = MaxPool2D(pool_size=(2, 2))(x)
-
-# Fourth conv block
-x = SeparableConv2D(filters=128, kernel_size=(3, 3), activation='relu', padding='same')(x)
-x = SeparableConv2D(filters=128, kernel_size=(3, 3), activation='relu', padding='same')(x)
-x = BatchNormalization()(x)
-x = MaxPool2D(pool_size=(2, 2))(x)
-x = Dropout(rate=0.2)(x)
-
-# Fifth conv block
-x = SeparableConv2D(filters=256, kernel_size=(3, 3), activation='relu', padding='same')(x)
-x = SeparableConv2D(filters=256, kernel_size=(3, 3), activation='relu', padding='same')(x)
-x = BatchNormalization()(x)
-x = MaxPool2D(pool_size=(2, 2))(x)
-x = Dropout(rate=0.2)(x)
-
-# FC layer
-x = Flatten()(x)
-x = Dense(units=512, activation='relu')(x)
-x = Dropout(rate=0.7)(x)
-x = Dense(units=128, activation='relu')(x)
-x = Dropout(rate=0.5)(x)
-x = Dense(units=64, activation='relu')(x)
-x = Dropout(rate=0.3)(x)
-
-# Output layer
-output = Dense(units=1, activation='sigmoid')(x)
-
-# Creating model and compiling
-model = Model(inputs=inputs, outputs=output)
-model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
-
-
-
-#################################
-##Load saved weights from original author
-model.load_weights('best_weights_kaggle_user_pneumonia2_0.hdf5')
-
-
-
-
-#################################
-##Confusion matrix report/Accuracy measures
-from sklearn.metrics import accuracy_score, confusion_matrix
-
-preds = model.predict(test_data)
-
-acc = accuracy_score(test_labels, np.round(preds))*100
-cm = confusion_matrix(test_labels, np.round(preds))
-tn, fp, fn, tp = cm.ravel()
-
-
-print('\nCONFUSION MATRIX FORMAT ------------------\n')
-print("[true positives    false positives]\n")
-print("[false negatives    true negatives]\n\n")
-
-print('CONFUSION MATRIX ------------------')
-print(cm)
-
-print('\nTEST METRICS ----------------------')
-precision = tp/(tp+fp)*100
-recall = tp/(tp+fn)*100
-specificity = tn/(tn+fp)*100 #Jordan_note: added specificity calculation 
-print('Accuracy: {}%'.format(acc))
-print('Precision: {}%'.format(precision))
-print('Recall/Sensitivity: {}%'.format(recall)) #Jordan_note: added sensitivity label
-print('Specificity {}%'.format(specificity)) #Jordan_note: added specificity calculation 
-print('F1-score: {}'.format(2*precision*recall/(precision+recall)))
-
-
+############
+#root cycle
+root.config(menu=menubar)
+root.mainloop()
